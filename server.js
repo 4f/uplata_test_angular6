2d9545e62f277ed2fcbc03f4451abbcfb84d0046
4f <ueuk11@gmail.com>
2018-10-12 17:41:13 +0300

    try6

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-const express = require('express');
 const path = require('path');
-var serveStatic = require('serve-static');
-
+const express = require('express');
 const app = express();
 
-app.use(serveStatic(__dirname + "/dist/uplata"));
 
-// app.get('/*', function(req,res) { res.sendFile(path.join(__dirname,'/dist/uplata/index.html')); });
+app.use(express.static(__dirname + '/dist/uplata'));
 
-var port = process.env.PORT || 5000;
-app.listen(port);
-console.log('server started '+ port);
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/uplata/index.html'));
+});
+
+
+app.listen(process.env.PORT || 5000);
