97926a7329d0ca08df3f21e1be8050e01312a4d3
4f <ueuk11@gmail.com>
2018-10-12 17:31:34 +0300

    f.server

--- server.js
+++ server.js
@@ -1,15 +1,13 @@
 const express = require('express');
 const path = require('path');
+var serveStatic = require('serve-static');
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static('./dist/uplata'));
+app.use(serveStatic(__dirname + "/dist/uplata"));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname,'/dist/uplata/index.html'));
-});
+// app.get('/*', function(req,res) { res.sendFile(path.join(__dirname,'/dist/uplata/index.html')); });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+var port = process.env.PORT || 5000;
+app.listen(port);
+console.log('server started '+ port);
